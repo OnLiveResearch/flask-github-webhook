1c1c418c6e1ee44f226a1657c61fe9a332b3af11
Patrick Welzel <patrick@kcirtap.de>
2014-06-29 11:31:59 +0200

    Fixed useless allowing GET requests and later on filter them out.

--- index.py
+++ index.py
@@ -12,50 +12,46 @@ from flask import Flask, request, abort
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=['POST'])
 def index():
     # Store the IP address blocks that github uses for hook requests.
     hook_blocks = requests.get('https://api.github.com/meta').json()['hooks']
 
-    if request.method == 'GET':
-        return 'Nothing to see here, move along ...'
+    # Check if the POST request if from github.com
+    for block in hook_blocks:
+        ip = ipaddress.ip_address(u'%s' % request.remote_addr)
+        if ipaddress.ip_address(ip) in ipaddress.ip_network(block):
+            break  # the remote_addr is within the network range of github
+    else:
+        abort(403)
 
-    elif request.method == 'POST':
-        # Check if the POST request if from github.com
-        for block in hook_blocks:
-            ip = ipaddress.ip_address(u'%s' % request.remote_addr)
-            if ipaddress.ip_address(ip) in ipaddress.ip_network(block):
-                break  # the remote_addr is within the network range of github
-        else:
-            abort(403)
+    if request.headers.get('X-GitHub-Event') == "ping":
+        return json.dumps({'msg': 'Hi!'})
+    if request.headers.get('X-GitHub-Event') != "push":
+        return json.dumps({'msg': "wrong event type"})
 
-        if request.headers.get('X-GitHub-Event') == "ping":
-            return json.dumps({'msg': 'Hi!'})
-        if request.headers.get('X-GitHub-Event') != "push":
-            return json.dumps({'msg': "wrong event type"})
+    repos = json.loads(io.open('repos.json', 'r').read())
 
-        repos = json.loads(io.open('repos.json', 'r').read())
-
-        payload = json.loads(request.data)
-        repo_meta = {
-            'name': payload['repository']['name'],
-            'owner': payload['repository']['owner']['name'],
-        }
-        match = re.match(r"refs/heads/(?P<branch>.*)", payload['ref'])
-        if match:
-            repo_meta['branch'] = match.groupdict()['branch']
-            repo = repos.get('{owner}/{name}/branch:{branch}'.format(**repo_meta), None)
-        else:
-            repo = repos.get('{owner}/{name}'.format(**repo_meta), None)
-        if repo and repo.get('path', None):
-            if repo.get('action', None):
-                for action in repo['action']:
-                    subprocess.Popen(action,
-                                     cwd=repo['path'])
-            else:
-                subprocess.Popen(["git", "pull", "origin", "master"],
+    payload = json.loads(request.data)
+    repo_meta = {
+        'name': payload['repository']['name'],
+        'owner': payload['repository']['owner']['name'],
+    }
+    match = re.match(r"refs/heads/(?P<branch>.*)", payload['ref'])
+    if match:
+        repo_meta['branch'] = match.groupdict()['branch']
+        repo = repos.get('{owner}/{name}/branch:{branch}'.format(**repo_meta), None)
+    else:
+        repo = repos.get('{owner}/{name}'.format(**repo_meta), None)
+    if repo and repo.get('path', None):
+        if repo.get('action', None):
+            for action in repo['action']:
+                subprocess.Popen(action,
                                  cwd=repo['path'])
-        return 'OK'
+        else:
+            subprocess.Popen(["git", "pull", "origin", "master"],
+                             cwd=repo['path'])
+    return 'OK'
 
 if __name__ == "__main__":
     try:
